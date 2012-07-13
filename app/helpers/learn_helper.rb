5d4e31a80ebe4ed27ee26513945cd7fdd029a287
Steven Ko <chunyuko85@gmaill.com>
2012-07-13 14:03:11 +0800

    change link_to helper

--- app/helpers/learn_helper.rb
+++ app/helpers/learn_helper.rb
@@ -1,5 +1,5 @@
 module LearnHelper
   def duplicate_link_to url
-    link_to url,url
+    link_to url,url, :onclick => "recordOutboundLink(this, 'Outbound Links', '#{url}');return false;"
   end
 end
