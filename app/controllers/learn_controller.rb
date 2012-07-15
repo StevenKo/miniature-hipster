342324f16a241b9da184890949889b25bcd6e931
Steven Ko <chunyuko85@gmaill.com>
2012-07-15 18:22:43 +0800

    java program

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -1,12 +1,14 @@
 class LearnController < ApplicationController
 
   def iphone_game
-    
   end
 
   def index
   end
 
   def backetball
+  end
+
+  def java
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LearnItTest1::Application.routes.draw do
   get 'learn-ios-games' => 'learn#iphone_game', :as => :learn_ios
+  get 'learn-java' => 'learn#java', :as => :learn_java
   get 'backetball' => "learn#backetball", :as => :learn_basketball
   root :to => 'learn#index'
 end
