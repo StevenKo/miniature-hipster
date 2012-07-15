9bd0c0197a3a7519e2494bba3414a30c4f59c382
Steven Ko <chunyuko85@gmaill.com>
2012-07-15 10:47:40 +0800

    change google analytics action

--- app/helpers/learn_helper.rb
+++ app/helpers/learn_helper.rb
@@ -1,5 +1,5 @@
 module LearnHelper
-  def duplicate_link_to url
-    link_to url,url, :onclick => "recordOutboundLink(this, 'Outbound Links', '#{url}');return false;"
+  def duplicate_link_to url, action
+    link_to url,url, :onclick => "recordOutboundLink(this, 'Outbound Links', '#{action}');return false;"
   end
 end
