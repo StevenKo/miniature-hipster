a88ae483cd320fb51820a0656b473deb7058e43e
Steven Ko <chunyuko85@gmaill.com>
2012-07-15 10:50:16 +0800

    skip asset precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,28 +23,28 @@ set :assets_dependencies, %w(app/assets lib/assets vendor/assets Gemfile.lock co
 
 namespace :deploy do
   
-  # namespace :assets do
+  namespace :assets do
 
-  #   desc <<-DESC
-  #     Run the asset precompilation rake task. You can specify the full path \
-  #     to the rake executable by setting the rake variable. You can also \
-  #     specify additional environment variables to pass to rake via the \
-  #     asset_env variable. The defaults are:
+    desc <<-DESC
+      Run the asset precompilation rake task. You can specify the full path \
+      to the rake executable by setting the rake variable. You can also \
+      specify additional environment variables to pass to rake via the \
+      asset_env variable. The defaults are:
 
-  #       set :rake,      "rake"
-  #       set :rails_env, "production"
-  #       set :asset_env, "RAILS_GROUPS=assets"
-  #       set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
-  #   DESC
-  #   task :precompile, :roles => :web, :except => { :no_release => true } do
-  #     from = source.next_revision(current_revision)
-  #     if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
-  #       run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-  #     else
-  #       logger.info "Skipping asset pre-compilation because there were no asset changes"
-  #     end
-  #   end
-  # end
+        set :rake,      "rake"
+        set :rails_env, "production"
+        set :asset_env, "RAILS_GROUPS=assets"
+        set :assets_dependencies, fetch(:assets_dependencies) + %w(config/locales/js)
+    DESC
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} #{assets_dependencies.join ' '} | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
 
   # task :copy_config_files, :roles => [:app] do
   #   db_config = "#{shared_path}/config/database.yml.production"
